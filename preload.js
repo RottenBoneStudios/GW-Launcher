c7b502862066ec9d44b16b96049e290bed886f2b
Anyelo120 <70180169+Anyelo120@users.noreply.github.com>
2025-09-11 01:32:52 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openProfileEditor: (name) => ipcRenderer.invoke('open-profile-editor', name)
-});
